d7fc06f1780fd2755266e5366179fe913de9c22a
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-14 17:57:34 +0900

    Update SymbolTable.java

--- src/main/java/com/github/tomokinakamaru/protocool/data/symboltable/SymbolTable.java
+++ src/main/java/com/github/tomokinakamaru/protocool/data/symboltable/SymbolTable.java
@@ -1,5 +1,6 @@
 package com.github.tomokinakamaru.protocool.data.symboltable;
 
 import java.util.LinkedHashMap;
+import org.antlr.v4.runtime.ParserRuleContext;
 
-public abstract class SymbolTable<T> extends LinkedHashMap<String, T> {}
+public abstract class SymbolTable<T extends ParserRuleContext> extends LinkedHashMap<String, T> {}
