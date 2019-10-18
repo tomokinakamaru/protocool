fea11a77255860031d086cb6ba6310ef76d00d02
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 23:21:31 +0900

    misc

--- src/main/java/com/github/tomokinakamaru/protocool/Protocool.java
+++ src/main/java/com/github/tomokinakamaru/protocool/Protocool.java
@@ -5,8 +5,15 @@ import com.github.tomokinakamaru.antlr4.utility.AbstractCompiler;
 import com.github.tomokinakamaru.antlr4.utility.Context;
 import com.github.tomokinakamaru.protocool.analysis.BuildTypeTable;
 import com.github.tomokinakamaru.protocool.analysis.Parse;
+import com.github.tomokinakamaru.protocool.analysis.automaton.AssignStateNumber;
+import com.github.tomokinakamaru.protocool.analysis.automaton.AssignStateReference;
+import com.github.tomokinakamaru.protocool.analysis.automaton.BuildChainAutomaton;
 import com.github.tomokinakamaru.protocool.analysis.automaton.BuildClassAutomaton;
-import com.github.tomokinakamaru.protocool.analysis.automaton.BuildExpressionAutomaton;
+import com.github.tomokinakamaru.protocool.analysis.automaton.FindConflict;
+import com.github.tomokinakamaru.protocool.analysis.automaton.PropagateParameter;
+import com.github.tomokinakamaru.protocool.analysis.automaton.RemoveFinalState;
+import com.github.tomokinakamaru.protocool.analysis.automaton.StoreReturnExpression;
+import com.github.tomokinakamaru.protocool.analysis.automaton.StoreStaticMethod;
 import java.util.Arrays;
 import java.util.List;
 import org.antlr.v4.runtime.CharStream;
@@ -24,7 +31,14 @@ public class Protocool extends AbstractCompiler {
     return Arrays.asList(
         new Parse(),
         new BuildTypeTable(),
-        new BuildExpressionAutomaton(),
-        new BuildClassAutomaton());
+        new BuildChainAutomaton(),
+        new StoreStaticMethod(),
+        new StoreReturnExpression(),
+        new BuildClassAutomaton(),
+        new PropagateParameter(),
+        new AssignStateReference(),
+        new RemoveFinalState(),
+        new FindConflict(),
+        new AssignStateNumber());
   }
 }
