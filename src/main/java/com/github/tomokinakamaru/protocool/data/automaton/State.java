dfa8a946096ec8f51cde30ba6767912c51211d06
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-19 01:43:15 +0900

    Update State.java

--- src/main/java/com/github/tomokinakamaru/protocool/data/automaton/State.java
+++ src/main/java/com/github/tomokinakamaru/protocool/data/automaton/State.java
@@ -1,5 +1,21 @@
 package com.github.tomokinakamaru.protocool.data.automaton;
 
 import com.github.tomokinakamaru.fsa.StateInterface;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ParameterContext;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ReferenceContext;
+import java.util.Set;
 
-public class State implements StateInterface {}
+public class State implements StateInterface {
+
+  public static final int INITIAL_NUMBER = 0;
+
+  public int number;
+
+  public ReferenceContext context;
+
+  public Set<ParameterContext> parameters;
+
+  public boolean hasParameters() {
+    return parameters != null;
+  }
+}
