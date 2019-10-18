d88784442ea8efd7540ea613bfcfe4cad849c90a
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 22:36:09 +0900

    Create AssignStateNumber.java

---
+++ src/main/java/com/github/tomokinakamaru/protocool/analysis/automaton/AssignStateNumber.java
@@ -0,0 +1,35 @@
+package com.github.tomokinakamaru.protocool.analysis.automaton;
+
+import static com.github.tomokinakamaru.protocool.data.automaton.State.INITIAL_NUMBER;
+
+import com.github.tomokinakamaru.protocool.analysis.abst.AutomatonAnalyzer;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ClassContext;
+import com.github.tomokinakamaru.protocool.data.automaton.Automaton;
+import com.github.tomokinakamaru.protocool.data.automaton.State;
+import com.github.tomokinakamaru.protocool.data.automaton.Transition;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class AssignStateNumber extends AutomatonAnalyzer {
+
+  @Override
+  protected void analyze(ClassContext ctx, Automaton a) {
+    List<State> queue = new ArrayList<>(a.initials);
+    Set<State> queuedStates = new HashSet<>(a.initials);
+
+    int n = INITIAL_NUMBER;
+    while (!queue.isEmpty()) {
+      State source = queue.remove(0);
+      source.number = n;
+      n += 1;
+      for (Transition transition : a.transitions) {
+        if (!queuedStates.contains(transition.destination)) {
+          queue.add(transition.destination);
+          queuedStates.add(transition.destination);
+        }
+      }
+    }
+  }
+}
