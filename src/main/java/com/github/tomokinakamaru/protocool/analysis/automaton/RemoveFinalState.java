20f0b3073512ed20996fe69827f640f84f0d2f08
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 22:35:23 +0900

    Create RemoveFinalState.java

---
+++ src/main/java/com/github/tomokinakamaru/protocool/analysis/automaton/RemoveFinalState.java
@@ -0,0 +1,14 @@
+package com.github.tomokinakamaru.protocool.analysis.automaton;
+
+import com.github.tomokinakamaru.protocool.analysis.abst.AutomatonAnalyzer;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ClassContext;
+import com.github.tomokinakamaru.protocool.data.automaton.Automaton;
+
+public class RemoveFinalState extends AutomatonAnalyzer {
+
+  @Override
+  protected void analyze(ClassContext ctx, Automaton a) {
+    a.transitions.removeAll(a.getTransitionsTo(a.finals));
+    a.finals.clear();
+  }
+}
