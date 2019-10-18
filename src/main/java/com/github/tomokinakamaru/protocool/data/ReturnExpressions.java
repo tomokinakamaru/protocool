986733d6109b11762facdcf0c665d97dd359a375
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 23:33:06 +0900

    misc

---
+++ src/main/java/com/github/tomokinakamaru/protocool/data/ReturnExpressions.java
@@ -0,0 +1,5 @@
+package com.github.tomokinakamaru.protocool.data;
+
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+
+public class ReturnExpressions extends ParseTreeProperty<String> {}
---
+++ src/main/java/com/github/tomokinakamaru/protocool/data/StaticMethods.java
@@ -0,0 +1,6 @@
+package com.github.tomokinakamaru.protocool.data;
+
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.MethodContext;
+import java.util.HashSet;
+
+public class StaticMethods extends HashSet<MethodContext> {}
