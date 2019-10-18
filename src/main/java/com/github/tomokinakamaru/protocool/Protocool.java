e30573790e65044d31dcddbec3424b90f7435797
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 14:32:35 +0900

    Update Protocool.java

--- src/main/java/com/github/tomokinakamaru/protocool/Protocool.java
+++ src/main/java/com/github/tomokinakamaru/protocool/Protocool.java
@@ -5,6 +5,8 @@ import com.github.tomokinakamaru.antlr4.utility.AbstractCompiler;
 import com.github.tomokinakamaru.antlr4.utility.Context;
 import com.github.tomokinakamaru.protocool.analysis.BuildTypeTable;
 import com.github.tomokinakamaru.protocool.analysis.Parse;
+import com.github.tomokinakamaru.protocool.analysis.automaton.BuildClassAutomaton;
+import com.github.tomokinakamaru.protocool.analysis.automaton.BuildExpressionAutomaton;
 import java.util.Arrays;
 import java.util.List;
 import org.antlr.v4.runtime.CharStream;
@@ -19,6 +21,10 @@ public class Protocool extends AbstractCompiler {
 
   @Override
   protected List<AbstractAnalyzer> analyses() {
-    return Arrays.asList(new Parse(), new BuildTypeTable());
+    return Arrays.asList(
+        new Parse(),
+        new BuildTypeTable(),
+        new BuildExpressionAutomaton(),
+        new BuildClassAutomaton());
   }
 }
