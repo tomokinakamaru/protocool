f9dfd15803702692caf85a375a2b30260b7fcb5b
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-19 04:06:14 +0900

    Update Symbol.java

--- src/main/java/com/github/tomokinakamaru/protocool/data/automaton/Symbol.java
+++ src/main/java/com/github/tomokinakamaru/protocool/data/automaton/Symbol.java
@@ -1,8 +1,46 @@
 package com.github.tomokinakamaru.protocool.data.automaton;
 
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.MethodContext;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ReferenceContext;
 import org.antlr.v4.runtime.ParserRuleContext;
 
 public class Symbol {
 
-  public Symbol(ParserRuleContext ctx) {}
+  private ParserRuleContext context;
+
+  public String normalizedText;
+
+  public Symbol(ParserRuleContext ctx) {
+    this.context = ctx;
+  }
+
+  public boolean isMethodContext() {
+    return context instanceof MethodContext;
+  }
+
+  public boolean isReferenceContext() {
+    return context instanceof ReferenceContext;
+  }
+
+  public MethodContext asMethodContext() {
+    return (MethodContext) context;
+  }
+
+  public ReferenceContext asReferenceContext() {
+    return (ReferenceContext) context;
+  }
+
+  @Override
+  public int hashCode() {
+    return normalizedText.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Symbol)) {
+      return false;
+    }
+    Symbol symbol = (Symbol) obj;
+    return normalizedText.equals(symbol.normalizedText);
+  }
 }
