1f2bdc1835067d7685be182ca0ae400b74b7bbff
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-19 13:36:28 +0900

    Create BuildNormalForm.java

---
+++ src/main/java/com/github/tomokinakamaru/protocool/analysis/tree/BuildNormalForm.java
@@ -0,0 +1,49 @@
+package com.github.tomokinakamaru.protocool.analysis.tree;
+
+import static com.github.tomokinakamaru.antlr4.utility.Stringifier.stringify;
+
+import com.github.tomokinakamaru.protocool.analysis.abst.tree.Listener;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ChainContext;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.MethodContext;
+import com.github.tomokinakamaru.protocool.analysis.antlr.SpecificationParser.ReferenceContext;
+import com.github.tomokinakamaru.protocool.data.NormalForms;
+import com.github.tomokinakamaru.protocool.data.ReturnExpressions;
+import com.github.tomokinakamaru.protocool.data.StaticMethods;
+
+public class BuildNormalForm extends Listener {
+
+  @Override
+  public void initialize() {
+    if (!has(NormalForms.class)) {
+      set(new NormalForms());
+    }
+  }
+
+  @Override
+  public void enterMethod(MethodContext ctx) {
+    super.enterMethod(ctx);
+  }
+
+  @Override
+  public void enterReference(ReferenceContext ctx) {
+    super.enterReference(ctx);
+  }
+
+  @Override
+  public void enterChain(ChainContext ctx) {
+    get(NormalForms.class).put(ctx, stringify(ctx.reference()));
+  }
+
+  @Override
+  public void exitMethod(MethodContext ctx) {
+    StringBuilder builder = new StringBuilder();
+    if (has(StaticMethods.class) && get(StaticMethods.class).contains(ctx)) {
+      builder.append("static").append(" ");
+    }
+    builder.append(stringify(ctx));
+    if (has(ReturnExpressions.class) && get(ReturnExpressions.class).get(ctx) != null) {
+      builder.append(" return ").append(get(ReturnExpressions.class).get(ctx));
+    }
+    get(NormalForms.class).put(ctx, builder.toString());
+  }
+}
