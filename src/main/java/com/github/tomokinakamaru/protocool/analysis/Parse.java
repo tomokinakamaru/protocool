1c1e2461e2166967e2827de52a6ef88bd777da4f
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-20 13:05:54 +0900

    Update Parse.java

--- src/main/java/com/github/tomokinakamaru/protocool/analysis/Parse.java
+++ src/main/java/com/github/tomokinakamaru/protocool/analysis/Parse.java
@@ -3,10 +3,13 @@ package com.github.tomokinakamaru.protocool.analysis;
 import com.github.tomokinakamaru.antlr4.utility.AbstractParser;
 import com.github.tomokinakamaru.protocool.analysis.antlr.GrammarLexer;
 import com.github.tomokinakamaru.protocool.analysis.antlr.GrammarParser;
+import com.github.tomokinakamaru.protocool.error.ParseError;
 import java.util.function.Function;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.TokenStream;
 
 public class Parse extends AbstractParser<GrammarParser> {
@@ -24,5 +27,10 @@ public class Parse extends AbstractParser<GrammarParser> {
   @Override
   protected Function<GrammarParser, ? extends ParserRuleContext> getRootContext() {
     return GrammarParser::file;
+  }
+
+  @Override
+  public void syntaxError(Recognizer r, Object o, int l, int c, String m, RecognitionException e) {
+    throw new ParseError(l, c, m);
   }
 }
