bf8e5ffba1504c41ce94dab59603153ecfef55e7
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-20 10:10:51 +0900

    Update StorePackage.java

--- src/main/java/com/github/tomokinakamaru/protocool/analysis/tree/StorePackage.java
+++ src/main/java/com/github/tomokinakamaru/protocool/analysis/tree/StorePackage.java
@@ -3,7 +3,7 @@ package com.github.tomokinakamaru.protocool.analysis.tree;
 import com.github.tomokinakamaru.protocool.analysis.abst.tree.Listener;
 import com.github.tomokinakamaru.protocool.analysis.antlr.GrammarParser.PackageContext;
 
-public final class StorePackage extends Listener {
+public class StorePackage extends Listener {
 
   @Override
   public void enterPackage(PackageContext ctx) {
